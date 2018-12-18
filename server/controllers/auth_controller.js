ca78f7f6de6f910719e70f6f85169fcd1a39e683
qqqy <frogasaurus@hotmail.com>
2018-12-18 15:01:05 -0700

    6/12

---
+++ server/controllers/auth_controller.js
@@ -0,0 +1,37 @@
+const users = require('../models/users')
+var id = 1
+
+module.exports = {
+  login: function(req, res, next){
+    const { username, password} = req.body
+    let match = users.find((userOb, i) => userOb.username === username && userOb.password === password)
+    if (match){req.session.user.username = username, res.status(200).send(match)} else { res.sendStatus(500)}
+  }
+  ,
+  register: function(req, res, next){
+    const { username, password} = req.body
+    if(username && password){
+      let newUser = {
+        id: id,
+        username,
+        password
+      }
+      console.log(`registering ${newUser}`)
+      users.push(newUser)
+      id++
+      console.log(`ID is now ${id}`)
+      res.status(200).send(newUser)
+    } else {
+      res.status(401).send('Invalid Username or Password')
+    }
+  }
+  ,
+  signout: function(req, res, next){
+    req.session.destroy()
+    res.status(200).send(req.session)
+  }
+  ,
+  getUser: function(req, res, next){
+    res.status(200).send(req.session.user)
+  }
+}
